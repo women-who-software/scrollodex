c5a00028fb7821ab504c9c14f41ba86eeb4ac73f
Lee-Ann-Mokoena <leeann.mokoena@gmail.com>
2021-10-14 10:16:40 -0500

    more fixes

--- src/cardsDisplay.jsx
+++ src/cardsDisplay.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./cardsDisplay.scss";
 
-<<<<<<< HEAD
 import ExampleCard from './cards/exampleCard/exampleCard';
 import IndigoCard from "./cards/indigoCard/indigoCard";
 import KristiCard from './cards/kristiCard/kristiCard';

c0b38a53eb86a3f4e3d8401c9b322dd77aa47106
Indigo Starr <indigojstarr@gmail.com>
2021-10-13 20:09:40 -0600

    removing notes from file

--- src/cardsDisplay.jsx
+++ src/cardsDisplay.jsx
@@ -4,8 +4,6 @@ import "./cardsDisplay.scss";
 import ExampleCard from "./cards/exampleCard/exampleCard";
 import IndigoCard from "./cards/indigoCard/indigoCard";
 
-// @Indigo - basically you use require(‘fs’) and require(‘path’) then parse the directory. Some assumptions will need to be made like directories that end in -Card for example. Then, in this case to keep with the example, find all files ending in -Card.jsx and assume that the component name matches the file name minus the extension
-
 function CardsDisplay(props) {
   return (
     <div className="cardholder">

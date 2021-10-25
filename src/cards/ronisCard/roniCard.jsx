bedf851cbc49d3137ebec8c89092b41be18ebec0
Roni <rtlockwoodwork@gmail.com>
2021-10-25 14:15:09 -0600

    added some styles

--- src/cards/ronisCard/roniCard.jsx
+++ src/cards/ronisCard/roniCard.jsx
@@ -6,18 +6,20 @@ import Roni from './roni-headshot.png';
 
 function ExampleCard() {
   return (
-    <div className="cardContainer">
+    <div className="cardContainer roni-container">
 
         <div className="card">
-            <img className="imageContainer" src={Roni} alt="example headshot of a woman" />
+            <img className="imageContainer roniImg" src={Roni} alt="example headshot of a woman" />
         </div>
 
-        <div className="card">
+        <div className="card roni-card">
             <h3>Roni Lockwood</h3>
             <p>Front-End Developer</p>
-            <a href="mailto:rtlockwoodwork@gmail.com">Email</a> <br />
-            <a href="roni.rocks" target="_blank">Portfolio Site</a> <br />
-            <a href="github.com/OlliesWorld" target="_blank">Github</a>
+            <a href="mailto:rtlockwoodwork@gmail.com" className="email">rtlockwoodwork@gmail.com</a> 
+            <div className="links">
+              <a href="github.com/OlliesWorld" target="_blank">Github</a>
+              <a href="roni.rocks" target="_blank">Portfolio</a> 
+            </div>
       </div>
 
     </div>

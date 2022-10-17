806528bf7343d78e2abdd66337727d00466e7d89
JAGrenier <jenniferagrenier@gmail.com>
2022-10-17 16:07:21 -0600

    refactor and style

--- src/cards/jennDiazCard/jennDiazCard.jsx
+++ src/cards/jennDiazCard/jennDiazCard.jsx
@@ -4,31 +4,36 @@ import headshot from './jenn.png';
 
 function JennDiazCard() {
 
-  const [ darkMode, setDarkMode ] = useState('light')
+  const [ darkMode, setDarkMode ] = useState(false)
   const toggleDarkMode = () => {
-    if (darkMode === 'light'){
-      setDarkMode('dark')
+    if (darkMode === false){
+      setDarkMode(true)
     } else {
-      setDarkMode('light')
+      setDarkMode(false)
     }
   }
 
   return (
-    <div className={darkMode === 'dark' ? "jennsCardContainerDark" : "jennsCardContainer"}>
+    <div className={darkMode ? "jennsCardContainerDark" : "jennsCardContainer"}>
       <div className="card">
           <img className="imageContainer" src={headshot} alt="headshot of a white women with brown hair smiling and standing outside" />
-          <button className="toggleButton" onClick={() => toggleDarkMode()}>Dark Mode</button>
       </div>
       <div className="card">
-          <h3 className={darkMode === 'dark' ? 'jdNameDark' : 'jdName'}>Jennifer Grenier Diaz</h3>
-          <p className={darkMode === 'dark' ? 'jdTextDark' : 'jdText'}>Software Engineer</p>
-          <p className={darkMode === 'dark' ? 'jdTextDark' : 'jdText'}>specializing in web accessibility and usability</p>
+          <h3 className={darkMode ? 'jdNameDark' : 'jdName'}>Jennifer Grenier Diaz</h3>
+          <p className={darkMode ? 'jdTextDark' : 'jdText'}>Software Engineer</p>
+          <p className={darkMode ? 'jdTextDark' : 'jdText'}>specializing in web accessibility and usability</p>
         <div className="jdButtons">
-          <a className={darkMode === 'dark' ? 'jdButtonDark' : 'jdButton'}href="https://github.com/jenndiaz">GitHub</a>
-          <a className={darkMode === 'dark' ? 'jdButtonDark' : 'jdButton'} href="https://www.linkedin.com/in/jennifergrenierdiaz/">LinkedIn</a>
-          <a className={darkMode === 'dark' ? 'jdButtonDark' : 'jdButton'} href="https://medium.com/@jagdiaz">Blog</a>
+          <a className={darkMode ? 'jdButtonDark' : 'jdButton'}href="https://github.com/jenndiaz">GitHub</a>
+          <a className={darkMode ? 'jdButtonDark' : 'jdButton'} href="https://www.linkedin.com/in/jennifergrenierdiaz/">LinkedIn</a>
+          <a className={darkMode ? 'jdButtonDark' : 'jdButton'} href="https://medium.com/@jagdiaz">Blog</a>
         </div>
       </div>
+      {!darkMode && 
+        <button className="toggleButton" onClick={() => toggleDarkMode()}>Dark Mode<span className="icon">&#x2610;</span></button>
+      }
+      {darkMode && 
+        <button className="toggleButtonDark" onClick={() => toggleDarkMode()}>Dark Mode<span className="icon">&#x2611;</span></button>
+      }
     </div>
   );
 }

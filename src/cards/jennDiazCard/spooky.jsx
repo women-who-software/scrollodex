947313e99c94581c226ac9b70d20f368e754b75e
jenndiaz <jennifergrenierdiaz@gmail.com>
2022-10-19 15:18:43 -0600

    add content to spooky

---
+++ src/cards/jennDiazCard/spooky.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import './jennDiazCard.scss';
+import ghost from './ghost.png';
+
+const Spooky = () => {
+
+  const [fadeProp, setFadeProp] = useState({
+    fade: 'fade-in', 
+  })
+  
+  useEffect(() => {
+    setInterval(() => {
+      setFadeProp({
+        fade: 'fade-out'
+      })
+    }, 3000);
+
+    return () => {}
+  }, [])
+
+  return (
+    <div className={`animation ${fadeProp.fade}`}>
+      <img className='ghost' src={ghost} alt='Small cartoon ghost fades in and then fades back out'></img>
+      <p className='mb-0 text-white'>Boo!</p>
+    </div>
+  );
+}
+
+export default Spooky;

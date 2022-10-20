3916182886937374f210d38703f38a743fbe0656
Katya <kittyserenakat@gmail.com>
2022-10-20 12:04:39 -0400

    Fix className

--- src/cards/katyaCard/katyaCard.jsx
+++ src/cards/katyaCard/katyaCard.jsx
@@ -8,15 +8,15 @@ function KatyaCard() {
 
         <div className="kat-card">
             <img className="kat-imageContainer" src={image} alt="example headshot of a woman" />
-            <div class="kat-image-border"></div>
+            <div className="kat-image-border"></div>
         </div>
 
         <div className="kat-card">
-            <h3 class="kat-header">Katya Sarmiento</h3>
-            <span class="role1">Full-Stack Developer</span><span class="role2">Lifelong Learner</span>
-            <a href="https://github.com/Kitkatnik/" target="_blank" rel="noreferrer" class="kat-button">GITHUB</a>
-            <a href="https://www.polywork.com/kitkatnik" target="_blank" rel="noreferrer" class="kat-button">POLYWORK</a>
-            <a href="https://www.linkedin.com/in/katyasarmiento/" target="_blank" rel="noreferrer" class="kat-button">LINKEDIN</a>
+            <h3 className="kat-header">Katya Sarmiento</h3>
+            <span className="role1">Full-Stack Developer</span><span className="role2">Lifelong Learner</span>
+            <a href="https://github.com/Kitkatnik/" target="_blank" rel="noreferrer" className="kat-button">GITHUB</a>
+            <a href="https://www.polywork.com/kitkatnik" target="_blank" rel="noreferrer" className="kat-button">POLYWORK</a>
+            <a href="https://www.linkedin.com/in/katyasarmiento/" target="_blank" rel="noreferrer" className="kat-button">LINKEDIN</a>
         </div>
 
     </div>

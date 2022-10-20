6a45404cd4b32d32d2c4fd5187fa16fff67b3502
hongphucdang1 <hpd.dang@gmail.com>
2022-10-19 21:52:17 -0400

    Add card

--- src/cardsDisplay.jsx
+++ src/cardsDisplay.jsx
@@ -21,6 +21,7 @@ import ParastouCard from "./cards/parastouCard/parastouCard";
 import AudreaCard from "./cards/audreasCard/audreaCard";
 import LovelaceCard from "./cards/lovelaceCard/LovelaceCard";
 import ExampleCard from "./cards/exampleCard/exampleCard";
+import HongCard from "./cards/hongCard/hongCard";
 
 function CardsDisplay() {
   const contributors = useContext(CardContext);
@@ -45,6 +46,7 @@ function CardsDisplay() {
     { name: "Parastou", component: <ParastouCard /> },
     { name: "Audrea", component: <AudreaCard /> },
     { name: "Ada", component: <LovelaceCard /> },
+    { name: "Hong", component: <HongCard /> },
   ];
 
   Object.keys(contributors).forEach((contributor) => {

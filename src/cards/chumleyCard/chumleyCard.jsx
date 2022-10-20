bdcd241908cfc561e5e138b4b9e5144240d5b034
a-u-m <dhuuumal.om@gmail.com>
2022-10-20 08:14:37 +0530

    Fixed bug

--- src/cards/chumleyCard/chumleyCard.jsx
+++ src/cards/chumleyCard/chumleyCard.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './chumleyCard.scss';
-import image from './chumley_headshot.png';
+import React from "react";
+import "./chumleyCard.scss";
+import image from "./chumley_headshot.png";
 
 function ChumleyCard() {
   return (
     <div className="cardContainer cardPresentation">
-
       <div className="cardInner">
         <div className="cardContent cardContentFront">
           <img className="imageContainer" src={image} alt="Rebecca Chumley" />
@@ -18,12 +17,24 @@ function ChumleyCard() {
 
         <div className="cardContent cardContentBack">
           <h3>Let's Connect</h3>
-          <a class="email" href="mailto:rebecca.chumley@gmail.com?subject=Women Who Code Boulder/Denver Scrollodex Email" rel='noreferrer'>Email Me</a>
-          <a class="linkedin" href="https://www.linkedin.com/in/rebecca-chumley/" target='_blank' rel='noreferrer'>LinkedIn</a>
+          <a
+            className="email"
+            href="mailto:rebecca.chumley@gmail.com?subject=Women Who Code Boulder/Denver Scrollodex Email"
+            rel="noreferrer"
+          >
+            Email Me
+          </a>
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/rebecca-chumley/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
         </div>
-      </div >
-    </div >
-
+      </div>
+    </div>
   );
 }
 

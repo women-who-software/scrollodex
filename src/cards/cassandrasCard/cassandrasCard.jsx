cc570398dbfa72e75ab7b4b589f73d01b7bd2a5a
Dom <9013550+domsbytes@users.noreply.github.com>
2022-10-24 19:32:31 -0700

    Update cassandrasCard.jsx

--- src/cards/cassandrasCard/cassandrasCard.jsx
+++ src/cards/cassandrasCard/cassandrasCard.jsx
@@ -12,7 +12,7 @@ function CassandrasCard() {
       <div className="card">
         <h3>Cassandra Reinhart</h3>
         <p>Full Stack Developer</p>
-        <a href="mailto:cassandrareinhart13@gmail.com" className="email">cassandrareinhart13@gmail</a></p>
+        <p><a href="mailto:cassandrareinhart13@gmail.com" className="email">cassandrareinhart13@gmail</a></p>
         <div>
         <a
           rel="noreferrer"

811720c272961a92d5609a4a67641669e86db66d
valeriecollinsdesign <valeriecollins270@gmail.com>
2022-10-28 11:32:54 -0600

    updated alt text to ada lovelaces card

--- src/cards/lovelaceCard/LovelaceCard.jsx
+++ src/cards/lovelaceCard/LovelaceCard.jsx
@@ -6,7 +6,7 @@ function LovelaceCard() {
   return (
     <div className="lovelaceCardContainer">
       <div className="card">
-        <img className="imageContainer" src={image} alt="lovelace-profile"/>
+        <img className="imageContainer" src={image} alt="drawing of a victorian era woman with a gold headband and long dark hair"/>
       </div>
       <div className="card card-info">
         <h3 className="name-h3">Ada Lovelace</h3>

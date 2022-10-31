4ec6ca9c509937f1fcad472062532c45777572d6
Jordan Levine <hi@mjordan.codes>
2022-10-30 23:45:31 -0600

    create styles to mimic Mondrian paintings

---
+++ src/cards/mjordanCard/mondrian.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./mondrian.scss";
+
+function Mondrian() {
+  return (
+    <div id="grid">
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+      <div class="cell"></div>
+    </div>
+  );
+}
+
+export default Mondrian;
